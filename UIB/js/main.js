423193976a268508c026f7b9b22beb3ea5cf21a9
Roman Shinderov <10poma10@mail.ru>
2020-02-09 14:53:02 +0300

    Доделана код выбора пары и ее выделения

--- UIB/js/main.js
+++ UIB/js/main.js
@@ -1,24 +1,21 @@
+"use struct";
 //var myDate = new Date();
-var myDate = new Date(2020, 1,10, 13,50);
+var myDate = new Date();
 
 Date.prototype.getWeek = function () {
     var date = new Date(this.getTime());
     date.setHours(0, 0, 0, 0);
-    // Thursday in current week decides the year.
     date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-    // January 4 is always in week 1.
     var week1 = new Date(date.getFullYear(), 0, 4);
-    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
     return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
         - 3 + (week1.getDay() + 6) % 7) / 7);
-}
+};
 
-// Returns the four-digit year corresponding to the ISO week of the date.
 Date.prototype.getWeekYear = function () {
     var date = new Date(this.getTime());
     date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
     return date.getFullYear();
-}
+};
 
 var tabel = {
     "Понедельник": [{w1: null, w2: {time: '11:20-12:50', subject: 'Открытые программные платформы(лб) Абрамов 1330'}},
@@ -60,17 +57,36 @@ var tabel = {
 (function main() {
     var tab = $('<div style=" position:fixed; top:0; left:0; width:100%; height:100%; display:flex; flex-direction: column; justify-content: stretch;"></div>');
     var mainTabel = $('<div style="margin: 3px; display: flex; flex-direction: column; border-bottom: 1pt solid black; justify-content: stretch; min-width: 900px; overflow: scroll;"></div>');
+    var msg = $('<div style="flex: 0 0 45px; margin: 3px; margin-left: 40px;"></div>');
     var week = Object.keys(tabel);
-    var newSubject = getNewSubject();
-    console.log(newSubject);
     for (var k = 0; k < week.length; k++) {
         var day = tabel[week[k]];
         mainTabel.append($('<tr style="background:#F8CBAD; display:flex; flex-direction:row; align-content:center; text-align:center; font-weight: 700; flex: 0 0 46px; border: 1pt solid black; border-bottom: none"><td style="width: 35px; flex:0 0 94px;"></td><td colspan="2" style="border-left: 1.0pt solid black; flex: 1 1 1px; text-decoration-style: solid; vertical-align: middle;"><p>' + week[k] + '</p></td></tr>'));
         mainTabel.append(addDay(day))
     }
+    tab.append(msg);
     tab.append(mainTabel);
     tab.append('<div style="flex: 1 1 1px;"></div>');
     $(document.body).append(tab);
+    setInterval(function () {
+        var newSubject = getNewSubject();
+        myDate = new Date();
+        var week = myDate.getWeek() % 2 !== 0 ? '1' : '2';
+        msg.html('<p>' + newSubject.ms + '</p>' + '<p>' + ((newSubject.ms2) ? newSubject.ms2 : '') + '</p>' + '<p>Сейчас ' + week + ' неделя</p>');
+        var tabelHtml = $(document.body).find('tr');
+        tabelHtml.removeClass('color-mode');
+        var weekDay = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница'], count = 0;
+        for (var g = 0; g < 5; g++) {
+            count++;
+            if (weekDay[g] === newSubject.day) {
+                count = count + newSubject.num;
+                break;
+            } else {
+                count = count + tabel[weekDay[g]].length;
+            }
+        }
+        $(tabelHtml[count]).addClass('color-mode');
+    }, 450);
 
     function addDay(day) {
         var HTMLtabel = "";
@@ -91,26 +107,95 @@ var tabel = {
     function getNewSubject() {
         var thisSubject = null;
         var day = myDate.getDay() - 1;
-        var week = myDate.getWeek();
+        var weekOrigin = myDate.getWeek() % 2 !== 0 ? 'w1' : 'w2';
+        var week = myDate.getWeek() % 2 !== 0 ? 'w1' : 'w2';
         if (day > 5 || day < 0) {
             return {ms: "Выходные"}
         }
         var weekDay = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница'];
+        var diff, house, mun;
         var dayInfo = tabel[weekDay[day]];
         for (var g = 0; g < dayInfo.length; g++) {
-            var subjectThis = dayInfo[g].w2 || dayInfo.w1;
+            var subjectThis = dayInfo[g][week];
+            if (subjectThis === null) continue;
+            if (subjectThis === 'replay') {
+                week = 'w2';
+                subjectThis = dayInfo[g][week];
+            }
             var time = subjectThis.time.split('-');
             var startDate = new Date(myDate.getTime()), endDate = new Date(myDate.getTime());
             startDate.setHours(time[0].split(':')[0], time[0].split(':')[1], 0, 0);
             endDate.setHours(time[1].split(':')[0], time[1].split(':')[1], 0, 0);
-            if (startDate.getTime() < myDate.getTime() && myDate.getTime() < endDate.getTime()) {
-                if (dayInfo[g].w1 === "replay" || dayInfo[g].w1 === null) {
-                    return dayInfo[g].w2;
-                } else {
-                    return dayInfo[g].w1;
+            if (startDate.getTime() <= myDate.getTime() && myDate.getTime() <= endDate.getTime()) {
+                var answer = {};
+                answer.day = weekDay[day];
+                answer.num = g;
+                answer.ms = "Сейчас идет \"" + subjectThis.subject + "\"";
+                answer.ms2 = '';
+                var nextPara = dayInfo[g + 1];
+                if (nextPara === null || nextPara === undefined) {
+                    answer.ms2 = 'Пары на сегодня закончились';
+                    return answer;
+                } else if (nextPara[weekOrigin] === null || nextPara[weekOrigin] === undefined) {
+                    answer.ms2 = 'Пары на сегодня закончились';
+                    return answer;
+                } else if (nextPara[weekOrigin] === 'replay') {
+                    weekOrigin = 'w2';
                 }
+                var timeTo = new Date(myDate.getTime());
+                var timeLocal = nextPara[weekOrigin].time.split('-');
+                startDate.setHours(timeLocal[0].split(':')[0], timeLocal[0].split(':')[1], 0, 0);
+                var diff = Math.abs(startDate.getTime() - myDate.getTime());
+                var house = Math.floor((diff / (1000 * 60 * 60)) % 24);
+                var mun = Math.floor(((diff / (1000 * 60)) % 60));
+                answer.ms2 = "Седующая пара \"" + nextPara[weekOrigin].subject + "\" через: " + house + ":" + mun;
+                return answer;
             }
         }
-        return null;
+        for (g = 1; g < dayInfo.length; g++) {
+            var subjectThis = dayInfo[g][week];
+            if (subjectThis === null) continue;
+            if (subjectThis === 'replay') {
+                week = 'w2';
+                subjectThis = dayInfo[g][week];
+            }
+            var time = subjectThis.time.split('-');
+            var time2 = dayInfo[g - 1][week].time.split('-');
+            var startDate = new Date(myDate.getTime()), endDate = new Date(myDate.getTime());
+            startDate.setHours(time2[1].split(':')[0], time2[1].split(':')[1], 0, 0);
+            endDate.setHours(time[0].split(':')[0], time[0].split(':')[1], 0, 0);
+            if (startDate.getTime() < myDate.getTime() && myDate.getTime() < endDate.getTime()) {
+                var answer = {};
+                answer.ms = "Сейчас перемена";
+                answer.day = weekDay[day];
+                answer.num = g;
+                var nextPara = dayInfo[g];
+                if (nextPara === null || nextPara === undefined) {
+                    answer.ms2 = 'Пары на сегодня закончились';
+                    return answer;
+                } else if (nextPara[weekOrigin] === null || nextPara[weekOrigin] === undefined) {
+                    answer.ms2 = 'Пары на сегодня закончились';
+                    return answer;
+                } else if (nextPara[weekOrigin] === 'replay') {
+                    weekOrigin = 'w2';
+                }
+                var diff = Math.abs(endDate.getTime() - myDate.getTime());
+                var house = Math.floor((diff / (1000 * 60 * 60)) % 24);
+                var mun = Math.floor(((diff / (1000 * 60)) % 60));
+                answer.ms2 = "Седующая пара \"" + nextPara[weekOrigin].subject + "\" через: " + house + ":" + mun;
+                return answer;
+            }
+        }
+        var firstDay = tabel[weekDay[day]][0].w1;
+        if (firstDay === 'replay' || firstDay === null) {
+            firstDay = tabel[weekDay[day]][0].w2;
+        }
+        endDate = new Date(myDate.getTime());
+        var timeL = firstDay.time.split('-');
+        endDate.setHours(timeL[0].split(':')[0], timeL[0].split(':')[1], 0, 0);
+        diff = Math.abs(endDate.getTime() - myDate.getTime());
+        house = Math.floor((diff / (1000 * 60 * 60)) % 24);
+        mun = Math.floor(((diff / (1000 * 60)) % 60));
+        return {ms: 'Пары еще не начались, до начала пар ' + house + ':' + mun};
     }
 }());
